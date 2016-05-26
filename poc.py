1914fa3812705d894df4e15f9c4d665d4472f7d6
Antonin Delpeuch <antonin@delpeuch.eu>
2016-05-26 11:26:19 +0200

    Fix bug

--- poc.py
+++ poc.py
@@ -14,8 +14,8 @@ from difflib import HtmlDiff
 # - keys are pairs
 #    - the first part (string) indicates the name of the identifier
 #    - the second part (bool) indicates whether it is
-#	* a template argument (as in {{cite journal|doi=10.1007/test}})
-#	* a template in the id= argument (as in {{cite journal|id={{citeseerx|10.1.1.104.7535}})
+#       * a template argument (as in {{cite journal|doi=10.1007/test}})
+#       * a template in the id= argument (as in {{cite journal|id={{citeseerx|10.1.1.104.7535}})
 # - the values are the regular expressions on the URLs that trigger these mappings
 #   The first parenthesis-enclosed group in these regular expressions should contain the id
 
@@ -54,6 +54,7 @@ def add_oa_links_in_references(text):
             nb_templates += 1
             link = get_oa_link(reference)
             if not link:
+                changed_templates.append((orig_template,None))
                 continue
 
             change = {}
@@ -61,25 +62,31 @@ def add_oa_links_in_references(text):
             argument_found = False
             for (name, is_arg), regex in template_arg_mappings.items():
                 match = regex.match(link)
-                if not match:
+                if not match or not match.group(1):
                     continue
                 argument_found = True
-                if is_arg and not template.has(name, ignore_empty=True):
-                    template.add(name, match.group(1))
-                    change[name] = (match.group(1),link)
-                    break
-		elif not is_arg and not template.has('id', ignore_empty=True):
-		    val = '{{%s|%s}}' % (name,match.group(1))
-		    template.add('id', val)
-		    change['id'] = (val,link)
-		    break
+                if is_arg:
+                    if not template.has(name, ignore_empty=True):
+                        template.add(name, match.group(1))
+                        change[name] = (match.group(1),link)
+                        break
+                    else:
+                        change['new_'+name] = (match.group(1),link)
+                elif not is_arg:
+                    if not template.has('id', ignore_empty=True):
+                        val = '{{%s|%s}}' % (name,match.group(1))
+                        template.add('id', val)
+                        change['id'] = (val,link)
+                        break
+                    else:
+                        change['new_'+name] = (match.group(1),link)
 
             if not argument_found and not template.has('url', ignore_empty=True):
                 template.add('url', link)
                 change['url'] = (link,link)
 
-            if change:
-                changed_templates.append((orig_template, change))
+            #if change:
+            changed_templates.append((orig_template, change))
     
     return unicode(wikicode), changed_templates, nb_templates
 
@@ -103,20 +110,20 @@ def get_text(page, max_hops=3):
 
 def get_page_over_api(page_name):
     r = requests.get('https://en.wikipedia.org/w/api.php', params={
-	'action':'query',
-	'titles':page_name,
-	'prop':'revisions',
-	'rvprop':'content',
-	'format':'json',})
+        'action':'query',
+        'titles':page_name,
+        'prop':'revisions',
+        'rvprop':'content',
+        'format':'json',})
     js = r.json()
     page = js.get('query',{}).get('pages',{}).values()[0]
     pagid = page.get('pageid', -1)
     if pagid == -1:
-	raise ValueError("Invalid page.")
+        raise ValueError("Invalid page.")
     text = page.get('revisions',[{}])[0]['*']
     return text
 
-def render_template(page_name):
+def render_template(page_name, this_url='#'):
     site = pywikibot.Site()
 
     with open('templates/skeleton.html','r') as f:
@@ -125,7 +132,7 @@ def render_template(page_name):
     try:
         #page = pywikibot.Page(site, page_name)
         #text, page_name = get_text(page)
-	text = get_page_over_api(page_name)
+        text = get_page_over_api(page_name)
     except (pywikibot.exceptions.Error, ValueError) as e:
         html = "<p><strong>Error:</strong> "+unicode(e)+"</p>"
         skeleton = skeleton.replace('OABOT_BODY_GOES_HERE', html)
@@ -139,21 +146,28 @@ def render_template(page_name):
     page_url = 'https://en.wikipedia.org/wiki/'+page_name #'https:'+page.permalink()
 
     html = '<h2>Results for page <a href="%s">OABOT_PAGE_NAME</a></h2>\n' % page_url
-    html += '<p>Processed: %s</p>\n' % (datetime.utcnow().isoformat())
+    html += '<p>Processed: %s (<a href="%s&refresh=true">refresh</a>)</p>\n' % (datetime.utcnow().isoformat(), this_url)
     html += '<p>Citation templates found: %d</p>\n' % nb_templates
 
     if not changed_templates:
         html += '<p><strong>No changes were made for this page.</strong></p>\n'
     else:
         # Render changes
-        html += '<h3>Templates changed (%d)</h3>\n' % len(changed_templates)
+
+        html += '<h3>Templates changed\n' # (%d)</h3>\n' % len(changed_templates)
         html += '<ol>\n'
         for template, change in changed_templates:
             html += '<li>'
             html += '<pre>'+unicode(template)+'</pre>\n'
+            if change == None:
+                html += '<strong>No OA version found.</strong>'
+                continue
             html += '<strong>Added:</strong>\n<ul>\n'
             for key, (val,link) in change.items():
-                html += '<li><span class="template_param">%s=' % key
+                if val.startswith('new_'):
+                    html += '<li>Already present: <span class="template_param">%s=' % key
+                else:
+                    html += '<li><span class="template_param">%s=' % key
                 html += '<a href="%s">%s</a>' % (link,val)
                 html += '</span></li>\n'
             html += '</ul>\n</li>\n'
@@ -171,5 +185,6 @@ def render_template(page_name):
 if __name__ == '__main__':
     page_name = sys.argv[1]
     print render_template(page_name).encode('utf-8')
+
 
 
